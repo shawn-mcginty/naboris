2c326873cda3c5d516f38101dd3c48b42ccfa602
Shawn McGinty <loltempast@gmail.com>
2020-04-08 14:51:50 -0500

    updating matomo script

--- docs-src/layouts/default.vue
+++ docs-src/layouts/default.vue
@@ -11,11 +11,11 @@
     <!-- Matomo -->
     <script type="text/javascript">
       var _paq = window._paq || [];
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      /* tracker methods like 'setCustomDimension' should be called before 'trackPageView' */
       _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
-      var u="//matomo.shawnmcginty.com/";
+      var u='//matomo.shawnmcginty.com/';
       _paq.push(['setTrackerUrl', u+'matomo.php']);
       _paq.push(['setSiteId', '1']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];

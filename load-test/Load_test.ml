2b9ad6710f1f2592a30c8cdde29b7e07083e7b88
Shawn McGinty <mcginty.shawn@gmail.com>
2020-10-01 17:19:23 -0500

    fixed load test server

--- load-test/Load_test.ml
+++ load-test/Load_test.ml
@@ -1,14 +1,19 @@
 open Naboris
 
-let main ()=
-  let nab_conf = ServerConfig.create ()
-    |> ServerConfig.setOnListen(fun () -> print_endline("Server started at 9997"))
-    |> ServerConfig.setRequestHandler(fun route req res -> match (Route.path route) with
-      | "static" :: path ->
-        let public_dir = Sys.getcwd () ^ "/load-test/public/" in
-        Naboris.Res.static public_dir path req res
-      | _ -> res |> Res.status 404 |> Res.text req "Not Found") in
-  listenAndWaitForever 9997 nab_conf
+let main () =
+  let nab_conf =
+    ServerConfig.make ()
+    |> ServerConfig.set_on_listen (fun () ->
+           print_endline "Server started at 9997")
+    |> ServerConfig.set_request_handler (fun route req res ->
+           match Route.path route with
+           | "static" :: path ->
+               let public_dir = Sys.getcwd () ^ "/load-test/public/" in
+               Naboris.Res.static public_dir path req res
+           | _ -> res |> Res.status 404 |> Res.text req "Not Found")
+  in
+  listen_and_wait_forever 9997 nab_conf
 
 let () = Lwt_engine.set (new Lwt_engine.libev ())
-let _ = Lwt_main.run(main ())
+
+let _ = Lwt_main.run (main ())
